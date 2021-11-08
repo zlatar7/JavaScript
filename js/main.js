dbe067b40d1f8be30a3e01ddbf2e87ade4692753
Guido Zlatar <guido_zlatar07@hotmail.com>
2021-11-08 15:58:55 -0300

    Clase 6/ 1° Entrega de pryecto

--- js/main.js
+++ js/main.js
@@ -12,10 +12,9 @@ function mostrar (monto){
 let cotizacion = dolarizar (distancia(kilometros, pesoKm), dolar).toFixed(2); 
 
 mostrar(cotizacion);
-*/
 
-let dias = parseInt(prompt("Cuantos días piensa alojarse?"))
 let personas = parseInt(prompt("Cuantas personas van a ser?"))
+let dias = parseInt(prompt("Cuantos días piensa alojarse?"))
 let habitacion = parseInt(prompt("Cuantas habitaciones necesitas?"))
 
 function Disponibilidad (d, p, h) {
@@ -31,5 +30,26 @@ function Disponibilidad (d, p, h) {
 
 const hotel = new Disponibilidad (dias, personas, habitacion);
 
-hotel.reserva();
+hotel.reserva(); */
 
+                    // ARRAY DE OBJETOS CON LAS HABITACIONES
+
+const arrayHabitacion = [ {nombre: "SIMPLE", capacidad: 1, disponible: "Hay disponilidad"},
+                    {nombre: "DOBLE", capacidad: 2, disponible: "No hay disponilidad"},
+                    {nombre: "TRIPLE", capacidad: 3, disponible: "No hay disponilidad"},
+                    {nombre: "CUADRUPLE", capacidad: 4, disponible: "Hay disponilidad"},
+                    {nombre: "SUITE", capacidad: 2, disponible: "Hay disponilidad"},
+                    {nombre: "DUPLEX", capacidad: 4, disponible: "Hay disponilidad"},
+]
+
+                    //MUESTRA LAS HABITACIONES DISPONIBLES
+
+  for (const dia of arrayHabitacion){
+    document.write(dia.nombre + ": ");
+    document.write(dia.disponible + ". ");
+}
+        //ORDENAMIENTO DESCENDENTE DE ACUERDO A LA CAPACIDAD DE PERSONAS
+
+arrayHabitacion.sort( (a, b) => (a.capacidad > b.capacidad) ? 1 : -1);
+console.log(arrayHabitacion);
+                
